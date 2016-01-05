30f00ad1fea9f7147fc952230f1e10b7302e1644
taylorleh <taylorleh@mac.com>
2016-01-04 18:39:52 -0800

    adds stylesheet

--- app/entry.js
+++ app/entry.js
@@ -2,13 +2,14 @@ import { App } from './model.js';
 import ReactDOM from 'react-dom';
 import React from 'react';
 import Todos from './src/todos.jsx';
+import './styles/styles.css';
 
 var model = new App();
 
 // main function
 const main = () => {
   return ReactDOM.render(
-    <h1>hello</h1>,
+    <Todos model={model} />,
     document.getElementById('react-view')
   );
 
