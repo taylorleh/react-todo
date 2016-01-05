6a57c7982e0dbc05203ecc0c3cf7dafa26951a0a
taylorleh <taylorleh@mac.com>
2016-01-04 18:55:55 -0800

    adds form to add todo

--- app/src/todos.jsx
+++ app/src/todos.jsx
@@ -3,8 +3,32 @@ import React from 'react';
 
 export default React.createClass({
 
+  getInitialState() {
+    return { note: '' };
+  },
+
+  onChange(event) {
+    var val = event.target.value;
+    this.setState({ note: val });
+  },
+
+  addTodo() {
+    this.props.model.add(this.state.note);
+  },
+
+  eachNote(msg) {
+    
+  },
+
   render() {
-    return <h1>hello</h1>;
+    var todos = this.props.model.todos;
+    return (
+      <div className="container border-curved">
+      <input type="text" value={this.state.note} onChange={this.onChange} />
+      <button onClick={this.addTodo}>Add</button>
+      <ul>{ todos.map(this.eachNote) }</ul>
+      </div>
+    );
   }
 
 })
